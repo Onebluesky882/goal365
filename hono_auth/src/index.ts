a8244ea1141d0dc5fd3c1e365d3d0036bd5f39b2
Onebluesky882 <onebluesky882@outlook.com>
2026-01-12 17:04:15 +0700

    build : set log user

--- hono_auth/src/index.ts
+++ hono_auth/src/index.ts
@@ -22,7 +22,11 @@ export const auth = betterAuth({
     provider: "pg",
     schema,
   }),
-  trustedOrigins: ["http://localhost:3001", "https://goal365-production.up.railway.app" ,"https://goal365.club"],
+  trustedOrigins: [
+    "http://localhost:3001",
+    "https://goal365-production.up.railway.app",
+    "https://goal365.club",
+  ],
   emailAndPassword: {
     enabled: true,
   },
@@ -41,9 +45,48 @@ app.all("/api/auth/*", async (c) => {
 app.get("/", (c) => c.text("API OK"));
 const port = Number(process.env.PORT ?? 3000);
 
+app.post("/api/auth/sign-in", async (c) => {
+  const res = await auth.handler(c.req.raw);
+
+  // login success
+  if (res.status === 200) {
+    const session = await auth.api.getSession({
+      headers: c.req.raw.headers,
+    });
+
+    const playerId = session?.user?.id;
+    if (playerId) {
+      await logLoginToGo(c, playerId);
+    }
+  }
+
+  return res;
+});
+
 console.log(`🚀 Server running on http://localhost:${port}`);
 
 export default {
   port,
   fetch: app.fetch,
 };
+
+
+async function logLoginToGo(c: any, playerId: string) {
+  const ip =
+    c.req.header("cf-connecting-ip") ??
+    c.req.header("x-forwarded-for") ??
+    "unknown";
+
+  const ua = c.req.header("user-agent") ?? "";
+
+  await fetch(process.env.GO_API_URL + "/internal/player/login-log", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Internal-Secret": process.env.INTERNAL_SECRET!,
+      "X-Real-IP": ip,
+      "User-Agent": ua,
+    },
+    body: JSON.stringify({ player_id: playerId }),
+  });
+}
