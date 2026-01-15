17bd046b075ef08ba348c707f6b913271b7afb70
Onebluesky882 <onebluesky882@outlook.com>
2026-01-16 00:43:11 +0700

    build : table sportbook

---
+++ frontend/src/app/sportbook/page.tsx
@@ -0,0 +1,12 @@
+import Sportbook from "@/pre-load/Sportbook";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Sportbook />
+    </div>
+  );
+};
+
+export default page;
---
+++ frontend/src/pre-load/Sportbook.tsx
@@ -0,0 +1,13 @@
+"use client";
+import SportsOddsTable from "@/components/sportbook/SportbookTable";
+import React from "react";
+
+const Sportbook = () => {
+  return (
+    <div>
+      <SportsOddsTable />
+    </div>
+  );
+};
+
+export default Sportbook;
