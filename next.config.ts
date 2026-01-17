11916b42545637690360f7b51272eb3d7cd9ed41
Onebluesky882 <onebluesky882@outlook.com>
2026-01-17 14:14:34 +0700

    fix : config image domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["media.api-sports.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.api-sports.io",
+        pathname: "/**",
+      },
+    ],
   },
   async rewrites() {
     return [
