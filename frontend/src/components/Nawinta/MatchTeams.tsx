fd559a35007726a22a3117357246f7a50c69d0ef
Onebluesky882 <onebluesky882@outlook.com>
2026-01-17 11:52:10 +0700

    fix : config next image domain

--- frontend/src/components/Nawinta/MatchTeams.tsx
+++ frontend/src/components/Nawinta/MatchTeams.tsx
@@ -70,7 +70,13 @@ function TeamFullCard({ team, title }: { team: TeamData; title: string }) {
     <div className="border rounded-lg p-4 space-y-4 text-sm bg-card/50 ">
       {/* Header */}
       <div className="flex items-center gap-3">
-        <Image src={team.logo} className="h-10 w-10" alt={""} />
+        <Image
+          src={team.logo}
+          className="h-10 w-10"
+          alt={""}
+          width={100}
+          height={100}
+        />
         <div>
           <div className="font-bold">{team.name}</div>
           <div className="text-xs text-gray-500">{title}</div>
