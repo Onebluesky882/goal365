8ca281c28bdcee068581fdbf70d6b34f538d42c5
Onebluesky882 <onebluesky882@outlook.com>
2026-01-18 12:43:41 +0700

    fix : nawin

--- src/app/nawin/page.tsx
+++ src/app/nawin/page.tsx
@@ -4,16 +4,13 @@ import type { PredictionsRoot } from "../../../types/predictions";
 
 const Page = () => {
   const roots = data as PredictionsRoot[];
-  console.log("NEXT_PUBLIC_API_URL", process.env.NEXT_PUBLIC_API_URL);
-  const env = process.env.NEXT_PUBLIC_API_URL;
 
   return (
     <div>
-      <h1>{env}</h1>
       {roots.map((root, rootIndex) =>
         root.response.map((item, index) => (
           <PredictionView key={`${rootIndex}-${index}`} data={item} />
-        ))
+        )),
       )}
     </div>
   );
